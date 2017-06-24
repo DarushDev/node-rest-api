2ae1bba2af6907ce8edc0b6ad289c60319f4cb33
DarushDev <darushfaqiri@gmail.com>
2017-06-24 16:42:49 +0800

    Create bear schema, connect to mongoDB database using mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var config = require('./config.json');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -9,6 +10,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080; // set our port
+
+var mongoose = require('mongoose');
+//paste your own mongoDB database url instead. in my case it was:
+//mongodb://<dbuser>:<dbpassword>@ds135812.mlab.com:35812/mydb1
+mongoose.connect(config.dbUrl);
+
+var Bear = require('./app/models/bear');
 
 // <========== ROUTES FOR OUR API ==========>
 var router = express.Router(); // get an instance of the express Router
